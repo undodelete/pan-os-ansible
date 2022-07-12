708df5839c4d7768107903e18ffe507ba9ab4c01
Sean O'Brien <sean.obrien@eirevo.ie>
2022-07-12 10:25:30 +0000

    Resolve doc issues in module Create integration test for module

--- plugins/modules/panos_arp_info.py
+++ plugins/modules/panos_arp_info.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+# Copyright (C) 2022 eir evo
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import, division, print_function
 
@@ -61,26 +63,29 @@ options:
 
 EXAMPLES = """
 - name: Getting ARP details from all interfaces
-    panos_arp_info:
-        provider: '{{ provider }}'
-    register: arp_int_full
+  panos_arp_info:
+    provider: '{{ provider }}'
+  register: arp_int_full
+
 - name: Getting ARP IPs from all interfaces
-    panos_arp_info:
-        provider: '{{ provider }}'
-        ip_only: true
-    register: arp_all_ip
+  panos_arp_info:
+    provider: '{{ provider }}'
+    ip_only: true
+  register: arp_all_ip
+
 - name: Getting ARP details from a specific interface
-    panos_arp_info:
-        provider: '{{ provider }}'
-        interface: 'ethernet1/1'
-    register: arp_int_full
+  panos_arp_info:
+    provider: '{{ provider }}'
+    interface: 'ethernet1/1'
+  register: arp_int_full
+
 - name: Getting ARP IPs from a specific interface with status complete
-    panos_arp_info:
-        provider: '{{ provider }}'
-        ip_only: true
-        interface: 'ethernet1/1'
-        status: 'complete'
-    register: arp_int_ip
+  panos_arp_info:
+    provider: '{{ provider }}'
+    ip_only: true
+    interface: 'ethernet1/1'
+    status: 'complete'
+  register: arp_int_ip
 """
 
 RETURN = """
